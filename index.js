9f4fa9a01ee3049e6bebbee47b97610dd20e7936
Ghazal Asaady <asaadyghazal@gmail.com>
2021-10-06 13:23:38 +0200

    almost ready just some responsive design left

---
+++ index.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const body = document.querySelector("body")
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    body.style.filter = "filter: blur(2px)"
+    
+}
