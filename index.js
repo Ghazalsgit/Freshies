cd0cf3496b2b23553281fdf9b8853bf43ea45630
Ghazal Asaady <asaadyghazal@gmail.com>
2021-10-13 11:27:59 +0200

    Remove parallax effect for older smartphones

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    body.style.filter = "filter: blur(2px)"
-    
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  body.style.filter = "filter: blur(2px)";
 }
